30d0d091d1df2d494123f7914864a6b4cb0948c8
scoobydoo-ma <sbndsharma@gmail.com>
2024-08-28 11:21:04 -0400

    modified to meet static code standard

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,32 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
-    all_emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    anger_score = all_emotions['anger']
-    disgust_score = all_emotions['disgust']
-    fear_score = all_emotions['fear']
-    joy_score = all_emotions['joy']
-    sadness_score = all_emotions['sadness']
+    if response.status_code == 200:
+        all_emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = all_emotions['anger']
+        disgust_score = all_emotions['disgust']
+        fear_score = all_emotions['fear']
+        joy_score = all_emotions['joy']
+        sadness_score = all_emotions['sadness']
+        emotions = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+        dominant_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = dominant_emotion
 
-    emotions = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score
-    }
-    dominant_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     return emotions
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,11 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_emotion_detection():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the scores for all 
+        emotions and provides the most dominant emotion.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -16,14 +25,16 @@ def sent_emotion_detection():
     dominant_emotion = response['dominant_emotion']
 
     if response['dominant_emotion'] is None:
-        return "Invalid input! Try again."
-    else:
-        return (f"For the given statement, the system response is 'anger': {anger}, "
+        return "Invalid text! Please try again!"
+    return (f"For the given statement, the system response is 'anger': {anger}, "
         f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
         f"The dominant emotion is {dominant_emotion}")
 
 @app.route("/")
 def render_index_page():
+    ''' This funtion initiates the rendering of the main application page 
+        over the Flask channel.
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
